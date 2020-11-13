76d6ff5ea0e803074ecb7cdfee38db3dd3399d3f
Doug Hellmann <dhellmann@redhat.com>
2020-11-13 13:28:36 -0500

    dumb web server to receive events
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

---
+++ receiver/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	bodyRaw, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("%s: %q\n", r.URL, bodyRaw)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "got it\n")
+}
+
+func main() {
+	var endpoint string
+	flag.StringVar(&endpoint, "endpoint", ":9090", "endpoint to listen on")
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+	fmt.Printf("listening on %s\n", endpoint)
+	log.Fatal(http.ListenAndServe(endpoint, nil))
+}
